08fd4bc850b52fdde81271dd8235b3b5efd12a52
cosmin <cosmin@atlasnet.ro>
2015-04-17 15:59:44 +0300

    OpenID Connect + Appengine

---
+++ src/main/java/ro/appenigne/web/framework/auth/DisconnectServlet.java
@@ -0,0 +1,22 @@
+package ro.appenigne.web.framework.auth;
+
+
+import ro.appenigne.web.framework.annotation.UrlPattern;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLDecoder;
+
+
+@UrlPattern("/disconnect")
+public class DisconnectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect(URLDecoder.decode(req.getParameter("redirect_to"), "UTF-8"));
+    }
+}
+
