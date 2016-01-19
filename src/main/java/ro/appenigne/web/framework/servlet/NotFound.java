f6a6498fc4f1b78059b3c68219216974aa574833
Bogdan <bogdan.nourescu@cbn-it.ro>
2016-01-19 19:45:35 +0200

    wrong implementation of NotFound. It was entering on / servlet

--- src/main/java/ro/appenigne/web/framework/servlet/NotFound.java
+++ src/main/java/ro/appenigne/web/framework/servlet/NotFound.java
@@ -2,23 +2,13 @@ package ro.appenigne.web.framework.servlet;
 
 import ro.appenigne.web.framework.annotation.UrlPattern;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @UrlPattern({"/bower_components"})
-public class NotFound extends HttpServlet {
-    private static final long serialVersionUID = -2354428535420248668L;
+public class NotFound extends AbstractIController {
 
     @Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.sendError(HttpServletResponse.SC_NOT_FOUND, "NotFound");
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
-	}
+    public void execute() throws Exception {
+        resp.sendError(HttpServletResponse.SC_NOT_FOUND, "NotFound");
+    }
 }
